6e95dbd6963039e521c1b316809cadd6d5233b18
Lior Abir <liorabir909@gmail.com>
2022-07-27 16:44:13 +0300

    connedted to atlas

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+	// 'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+	dbURL: 'mongodb+srv://boardsDB:1234@cluster0.vbexk.mongodb.net/?retryWrites=true&w=majority',
 }
